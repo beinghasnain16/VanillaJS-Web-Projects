27487027c2439e9a3ec67cf2488da00ea9ba3165
Hasnain Ali <beinghasnain16@gmail.com>
2020-06-05 03:22:43 +0500

    add check email function

--- 1. Form Validator/script.js
+++ 1. Form Validator/script.js
@@ -41,9 +41,21 @@ function checkLength(input, min, max) {
     }
 }
 
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    
+    if (!re.test(input.value.trim())) {
+        showError(input,'Email is not valid');
+    }
+    else {
+        showSuccess(input);
+    }
+    }
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();    
     checkRequired([username,email,password,password2]);
     checkLength(username, 4, 15);
     checkLength(password, 6, 18);
+    checkEmail(email);
 })
